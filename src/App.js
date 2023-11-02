9a0410cf6b395bdf7a7813f1a10bf2109831c419
javier-tp <javier.tapia.cvl@gmail.com>
2023-11-02 19:30:45 -0300

    funcionaaaa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import GamePage from './GamePage';
 import ClientView from './components/ClientView';
@@ -14,7 +14,7 @@ function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/party-cards-web-app" element={<HomePage />} />
+				<Route path="/" element={<HomePage />} />
 				<Route path="/game/:packName" element={<GamePage socket={socket} />} />
 				<Route path="/clientserver" element={<ClientView socket={socket} />} />
 			</Routes>
